a392b632a252e6ada2e5784439a87560fe672c19
Salgueirinho <goncalosalgueirinho842@gmail.com>
2022-11-18 14:23:55 +0000

    Add random playername

--- app/src/main/java/pt/isec/amov/mathit/model/data/Data.kt
+++ app/src/main/java/pt/isec/amov/mathit/model/data/Data.kt
@@ -1,10 +1,9 @@
 package pt.isec.amov.mathit.model.data
 
 class Data {
-    var playerName: String? = null
+    var playerName: String? = "Player" + (1..99999).shuffled().last()
         set(value) {
             if(!value.isNullOrEmpty())
                 field = value
         }
-        get() = field
 }
